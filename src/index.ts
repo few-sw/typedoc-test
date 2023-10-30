b4d78a0445a0ef9007303d13ed6270223743eaed
few-sw <149263800+few-sw@users.noreply.github.com>
2023-10-30 09:50:52 -0500

    add typedoc package and add comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,23 @@
 import { Greeter } from './utils'
 
+/**
+ * 
+ * This function returns the sum of two numbers
+ * 
+ * @param a first number
+ * @param b second number
+ * @returns a total of two numbers
+ */
 function mySum(a: number, b: number) {
     return b + a;
 }
 
+/**
+ * 
+ * This function returns the sum of three numbers, and one number is optional.
+ * 
+ * @returns a total of two numbers
+ */
 let calTotal: (a: number, b: number, c?: number) => number;
 calTotal = (num1: number, num2: number) => {
     return num1 + num2
--- src/utils/greeter.ts
+++ src/utils/greeter.ts
@@ -1,10 +1,21 @@
+/**
+ * This class greets new customers
+ */
 export class Greeter {
     public username: string = "Jack"
 
+    /**
+     * This function greets the user
+     */
     public greet() {
         console.log(`hi ${this.username}`)
     }
 
+    /**
+     * This function updates the username
+     * @param username new username
+     * @returns returns the new username
+     */
     public greetNewUser(username: string) {
         this.username = username
 
